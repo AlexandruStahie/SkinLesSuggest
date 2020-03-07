eb3c1f4b469a40cabe9e42506dcb1caa7e923e23
AlexandruStahie <stahie.alexandru@gmail.com>
2020-03-07 15:19:34 +0200

    Config Server + EF Core DB Connection

---
+++ Server/SkinLesSuggest/Services/Implementations/TestService.cs
@@ -0,0 +1,27 @@
+﻿using SkinLesSuggest.Context;
+using SkinLesSuggest.Models;
+using SkinLesSuggest.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkinLesSuggest.Services.Implementations
+{
+    public class TestService : ITestService
+    {
+        private readonly SkinLesSuggestContext _dbContext;
+
+        public TestService(SkinLesSuggestContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddTestRecord()
+        {
+            Test test = new Test() { Id = Guid.NewGuid() };
+            await _dbContext.Tests.AddAsync(test);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
