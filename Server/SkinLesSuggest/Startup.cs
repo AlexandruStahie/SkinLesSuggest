e64633dd4717dd6046ad8e247d83e1d0480f775c
AlexandruStahie <stahie.alexandru@gmail.com>
2020-03-07 21:36:30 +0200

    swagger integrations

--- Server/SkinLesSuggest/Startup.cs
+++ Server/SkinLesSuggest/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SkinLesSuggest.Context;
 using SkinLesSuggest.Services.Implementations;
 using SkinLesSuggest.Services.Interfaces;
@@ -23,6 +24,11 @@ namespace SkinLesSuggest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkinLesSuggest API", Version = "v1" });
+            });
+
             services.AddRazorPages();
             services.AddControllers();
             services.AddMvc();
@@ -37,10 +43,11 @@ namespace SkinLesSuggest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseStaticFiles();
             app.UseHttpsRedirection();
@@ -49,6 +56,13 @@ namespace SkinLesSuggest
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkinLesSuggest V1");
             });
         }
     }
